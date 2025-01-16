3c808e7aa8a1f17a6ee65065a3b048341b111cdc
Aw1ks <aw1ksss331@gmail.com>
2025-01-16 20:55:33 +0300

    Update

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
 
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+event1 = datetime.datetime(year=1920, month=12, day=24, hour=23)
+event2 = datetime.datetime.now()
+
+age_of_wineeeee = event1 - event2
+age_of_wineeeeeeeeeeeeeeee = age_of_wineeeee.days()
+
+rendered_page = template.render(
+    age_of_wine=age_of_wineeeeeeeeeeeeeeee,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
